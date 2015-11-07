d700526df7abb935a4adae79f6200f78676c65eb
Jones <kirsten.jones@FT-MW4599.local>
2015-11-07 12:21:16 +0000

    get fs to render an file. additional endpoint renders an image

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 var http = require('http');
+var fs   = require('fs');
 var port = process.env.PORT || 8000;
 
 function handler(request, response) {
   // display HELLO WORLD when the user is on the home page
-  // the request object has a bunch of properties and information in it
+  // the request object has a bunch of properties and information in it. Including the .url property which updates with whatever is entered in the url      bar in the browser.
 
-  console.log(request);
-  
-  var url = request.url; // e.g. '/'
-  
-  if (url.length ===1) {
+  // console.log(request); to take a look at the request object. it's huge.
+
+  var url = request.url; // request.url is just automatically populated to be whatever url is typed in to the address bar
+
+  if (url === '/') {
+
+
+    fs.readFile(__dirname + '/index.html', function (error, index){
+    //sends the index back to the client!
+    //  '__dirname' refers to current directory
+
+    // Node functions - first argument often error so you can deal with it if there's any error
+    // Second argument is the content that's found by fs.readFile - we call it index in there. Then can refer to index in the bit that tells
+    // the response what to do with it.
+
     response.writeHead(200, {"Content-Type": "text/html"}); // makes the status code 200 get written, along with the content type text/html
-    response.end("HELLO WORLD!");                           
+
+    response.write('Whaaaaaaat?');
+    response.end(index);
+
+    });
+
+  } else if (url === '/cat') {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end('<img src="http://bit.ly/1WEEsZI" />')
   }
 }
 // use http module's method createServer, pass in the handler function, and tell it to listen at the port we defined.
